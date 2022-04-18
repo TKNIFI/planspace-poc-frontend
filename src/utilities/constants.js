40312001c38cf18a14c262f9ef781c051dd330be
Shahzaib Rao <raoshahzaib009@gmail.com>
2022-04-18 23:02:38 +0500

    minor fix

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,122 +1,122 @@
 const K = {
-  Network: {
-    URL: {
-      // Production
-      // Base: 'http://ninjirosoft.com:8080/',
-      // BaseAPI: 'http://ninjirosoft.com:8080/api',
-      // TenantURL: (domainPrefix = '') => {
-      //     return 'http://' + domainPrefix + '.' + 'ninjirosoft.com:8080/api'
-      // },
-      // Client: {
-      //     BaseHost: 'ninjirosoft.com',
-      //     BasePort: '80',
-      // },
+    Network: {
+        URL: {
+            // Production
+            // Base: 'http://ninjirosoft.com:8080/',
+            // BaseAPI: 'http://ninjirosoft.com:8080/api',
+            // TenantURL: (domainPrefix = '') => {
+            //     return 'http://' + domainPrefix + '.' + 'ninjirosoft.com:8080/api'
+            // },
+            // Client: {
+            //     BaseHost: 'ninjirosoft.com',
+            //     BasePort: '80',
+            // },
 
-      // Development
-      Base: "https://planspace.herokuapp.com/",
-      BaseAPI: "hhttps://planspace.herokuapp.com/api",
-      Timeout: 1000,
-      TenantURL: (domainPrefix = "") => {
-        return "http://" + domainPrefix + "planspace.herokuapp.com/api";
-      },
-      Client: {
-        BaseHost: "planspace.herokuapp.com",
-        BasePort: "80",
-      },
+            // Development
+            Base: "https://planspace.herokuapp.com/",
+            BaseAPI: "https://planspace.herokuapp.com/api",
+            Timeout: 1000,
+            TenantURL: (domainPrefix = "") => {
+                return "http://" + domainPrefix + "planspace.herokuapp.com/api";
+            },
+            Client: {
+                BaseHost: "planspace.herokuapp.com",
+                BasePort: "80",
+            },
 
-      Protocol: "http",
+            Protocol: "http",
 
-      // Tenant
-      GetTenant: "/tenant/get",
+            // Tenant
+            GetTenant: "/tenant/get",
 
-      //register
-      Register: "/auth/register/",
+            //register
+            Register: "/auth/register/",
 
-      // Assignment
-      LoginUser: "/auth/login/",
+            // Assignment
+            LoginUser: "/auth/login/",
 
-      //create company
-      CreatCompany: "/company/",
+            //create company
+            CreatCompany: "/company/",
 
-      //get company
-      GetCompany: "/company/",
+            //get company
+            GetCompany: "/company/",
 
-      //create Locations
-      CreateLocation: "/location/",
+            //create Locations
+            CreateLocation: "/location/",
 
-      //get Locations
-      GetLocations: "/location/",
+            //get Locations
+            GetLocations: "/location/",
 
-      //update Location
-      UpdateLocation: "/locations/:id/update/",
+            //update Location
+            UpdateLocation: "/locations/:id/update/",
 
-      // delete Location
-      DeleteLocation: "/locations/:id/delete/",
+            // delete Location
+            DeleteLocation: "/locations/:id/delete/",
+        },
+        Method: {
+            GET: "GET",
+            POST: "POST",
+            DELETE: "DELETE",
+            UPDATE: "UPDATE",
+        },
+        Header: {
+            ContentType: "Content-Type",
+            ApplicationJson: "application/json",
+            Default: (token = "") => ({
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: "Bearer " + token,
+            }),
+            Authorization: (token = "") => ({
+                Authorization: "Bearer " + token,
+            }),
+            Type: {
+                Json: "json",
+                File: "file",
+            },
+        },
+        Default: {
+            AssignmentStatusID: 1,
+            ResourceAllocationPercentage: 100,
+            ResourceAllocationType: "percentage",
+            WorkItem: "",
+            Error: "Opps, an error occurred!",
+        },
+        StatusCode: {
+            Unauthorized: 401,
+        },
     },
-    Method: {
-      GET: "GET",
-      POST: "POST",
-      DELETE: "DELETE",
-      UPDATE: "UPDATE",
-    },
-    Header: {
-      ContentType: "Content-Type",
-      ApplicationJson: "application/json",
-      Default: (token = "") => ({
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: "Bearer " + token,
-      }),
-      Authorization: (token = "") => ({
-        Authorization: "Bearer " + token,
-      }),
-      Type: {
-        Json: "json",
-        File: "file",
-      },
-    },
-    Default: {
-      AssignmentStatusID: 1,
-      ResourceAllocationPercentage: 100,
-      ResourceAllocationType: "percentage",
-      WorkItem: "",
-      Error: "Opps, an error occurred!",
-    },
-    StatusCode: {
-      Unauthorized: 401,
-    },
-  },
-  Actions: {
-    // General part of action
-    CREATE: "CREATE",
-    UPSERT: "UPSERT",
-    DELETE: "DELETE",
-    DELETE_ALL: "DELETE_ALL",
-    SET: "SET",
+    Actions: {
+        // General part of action
+        CREATE: "CREATE",
+        UPSERT: "UPSERT",
+        DELETE: "DELETE",
+        DELETE_ALL: "DELETE_ALL",
+        SET: "SET",
 
-    // Settings
-    UPSERT_SETTING: "UPSERT_SETTING",
-  },
-
-  Cookie: {
-    Key: {
-      Token: "token",
-      Tenant: "tenant",
-      UserId: "user_id",
+        // Settings
+        UPSERT_SETTING: "UPSERT_SETTING",
     },
-  },
 
-  GuestPages: [
-    "/login",
-    "/authentication/register",
-    "/authentication/reset-password",
-    "/subscription/plans",
-  ],
+    Cookie: {
+        Key: {
+            Token: "token",
+            Tenant: "tenant",
+            UserId: "user_id",
+        },
+    },
 
-  Roles: {
-    Admin: "Admin",
-    User: "User",
-  },
+    GuestPages: [
+        "/login",
+        "/authentication/register",
+        "/authentication/reset-password",
+        "/subscription/plans",
+    ],
+
+    Roles: {
+        Admin: "Admin",
+        User: "User",
+    },
 };
 
 export default K;
