d339b2e533da224009b2ad2b8f8547fe905d063c
asimmaqsood57 <asimmaqsood57@gmail.com>
2022-07-28 08:06:53 +0500

    fix styling issue in password meter

--- src/utilities/generatePassword.js
+++ src/utilities/generatePassword.js
@@ -1,48 +1,51 @@
 // eslint-disable-next-line no-extend-native
-String.prototype.pick = function(min, max) {
-  var n, chars = '';
+String.prototype.pick = function (min, max) {
+  var n,
+    chars = "";
 
-  if (typeof max === 'undefined') {
-      n = min;
+  if (typeof max === "undefined") {
+    n = min;
   } else {
-      n = min + Math.floor(Math.random() * (max - min + 1));
+    n = min + Math.floor(Math.random() * (max - min + 1));
   }
 
   for (var i = 0; i < n; i++) {
-      chars += this.charAt(Math.floor(Math.random() * this.length));
+    chars += this.charAt(Math.floor(Math.random() * this.length));
   }
 
   return chars;
 };
 
-
 // Credit to @Christoph: http://stackoverflow.com/a/962890/464744
 // eslint-disable-next-line no-extend-native
-String.prototype.shuffle = function() {
-  var array = this.split('');
-  var tmp, current, top = array.length;
+String.prototype.shuffle = function () {
+  var array = this.split("");
+  var tmp,
+    current,
+    top = array.length;
 
-  if (top) while (--top) {
+  if (top)
+    while (--top) {
       current = Math.floor(Math.random() * (top + 1));
       tmp = array[current];
       array[current] = array[top];
       array[top] = tmp;
-  }
+    }
 
-  return array.join('');
+  return array.join("");
 };
 
-var specials = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numbers = '0123456789';
+var specials = "!@#$%^&*()_+{}:\"<>?|[];',./`~";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
 
 var all = specials + lowercase + uppercase + numbers;
 
-var password = '';
+var password = "";
 password += specials.pick(1);
 password += lowercase.pick(1);
 password += uppercase.pick(1);
 password += all.pick(3, 10);
 
-export {password as generatedPassword}
+export { password as generatedPassword };
